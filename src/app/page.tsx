43e3e3f0bf38d8b9742649dff416e400d055612a
chuckhalliday <charleshallidayclarkii@gmail.com>
2023-11-16 23:15:15 -0800

    Add Google Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,62 @@
+'use client'
+
+import { useState, useEffect } from "react";
 import { Hero, Tabs } from "@/components";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = 'https://bpyktwagkbpnqtwlekfa.supabase.co'
+const supabase = createClient(supabaseUrl, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJweWt0d2Fna2JwbnF0d2xla2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMDIxODEsImV4cCI6MjAxNTc3ODE4MX0.J0yA4SnftZnw7zHpKX8QCZIIMFin49R5fPjxJMAGiow')
 
 export default function Home() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const login = async () => {
+    if (!authenticated) {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+          queryParams: {
+            access_type: 'offline',
+            prompt: 'consent',
+          },
+        },
+      });
+      if (!error) {
+        setAuthenticated(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      const { data: { session } } = await supabase.auth.getSession()
+      if (session) {
+        setAuthenticated(true);
+      }
+    };
+
+    checkAuthentication();
+  }, []);
+
+
+  const logout = async () => {
+    let { error } = await supabase.auth.signOut()
+    setAuthenticated(false)
+  }
+  
   return (
-    <main className="overflow-hidden">
-      <Hero />
-      <div className="container">
-        <Tabs />
-      </div>
-    </main>
+    <div>
+      {authenticated ? (
+      <main className="overflow-hidden">
+        <Hero />
+        <div className="container">
+          <Tabs />
+          <button onClick={logout}>Log Out</button>
+        </div>
+      </main>
+      ) : (
+        <button onClick={login}>Log In with Google</button>
+      )}
+    </div>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className="hero">
-        <h1 className="hero__title flex-center">Whammy Central</h1>
+        <h1 className="flex-center hero__title">Whammy Central</h1>
     </div>
   )
 }
