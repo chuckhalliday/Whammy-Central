c1af4fcc25057202c3238884dda7ae22d04ce62e
chuckhalliday <charleshallidayclarkii@gmail.com>
2023-11-16 21:44:28 -0800

    First Commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <h1 className="hero__title flex-center">Whammy Central</h1>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import Hero from './Hero';
+import Tabs from './Tabs';
+import Songs from './Songs';
+
+export {
+    Hero,
+    Tabs,
+    Songs
+}
