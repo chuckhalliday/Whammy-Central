685f13af6c032acbbdce24f6a8ef3235ccac2650
chuckhalliday <charleshallidayclarkii@gmail.com>
2023-11-19 23:34:19 -0800

    Adjust icon

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div className='hero'>
-      <Image src={logo} alt='whammy central logo' width={250} height={250} />
       <h1 className='flex-center hero__title'>Whammy Central</h1>
+      <Image className ='hero__image' src={logo} alt='whammy central logo' />
     </div>
   );
 };
