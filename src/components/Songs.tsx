0015dba9c4bb05e64454e7aaad4e84de679b71c7
chuckhalliday <charleshallidayclarkii@gmail.com>
2023-11-19 22:37:02 -0800

    Add profile and artist components

--- src/components/Songs.tsx
+++ src/components/Songs.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { supabase } from '@/app/page'
+
+const loadSongs = async() => {
+  let { data: Songs, error } = await supabase
+  .from('Songs')
+  .select('*')
+  return Songs
+}
 
 const Songs = () => {
+
+  const songs = loadSongs()
+  console.log(songs)
+
   return (
     <div>Songs</div>
   )
