03c67e6416e8d06d921ab4f4defb0a049cd5c10c
chuckhalliday <charleshallidayclarkii@gmail.com>
2023-11-19 23:58:34 -0800

    Map song data

--- src/components/Songs.tsx
+++ src/components/Songs.tsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://bpyktwagkbpnqtwlekfa.supabase.co'
 const supabase = createClient(supabaseUrl, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJweWt0d2Fna2JwbnF0d2xla2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMDIxODEsImV4cCI6MjAxNTc3ODE4MX0.J0yA4SnftZnw7zHpKX8QCZIIMFin49R5fPjxJMAGiow')
 
-const loadSongs = async() => {
-  let { data: Songs, error } = await supabase
-  .from('Songs')
-  .select('*')
-  return Songs
-}
-
 const Songs = () => {
+  const [userSongs, setUserSongs] = useState<any[]>([]);
 
-  const songs = loadSongs()
-  console.log(songs)
+  const loadSongs = async () => {
+    let { data: songs, error } = await supabase
+    .from('songs')
+    .select()
+    if (songs) {
+      setUserSongs(songs)
+    } else {
+      return []
+    }
+  }
+
+  loadSongs()
 
   return (
-    <div>Songs</div>
+    <div>
+      <ul>
+        {userSongs.map((song) => (
+          <li key={song.id}>{song.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
