18b7c07010030657fe870f3c98fa2dd6bb31ce56
chuckhalliday <charleshallidayclarkii@gmail.com>
2023-11-19 22:39:42 -0800

    Fix supabase export error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Hero, Tabs } from "@/components";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = 'https://bpyktwagkbpnqtwlekfa.supabase.co'
-export const supabase = createClient(supabaseUrl, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJweWt0d2Fna2JwbnF0d2xla2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMDIxODEsImV4cCI6MjAxNTc3ODE4MX0.J0yA4SnftZnw7zHpKX8QCZIIMFin49R5fPjxJMAGiow')
+const supabase = createClient(supabaseUrl, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJweWt0d2Fna2JwbnF0d2xla2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMDIxODEsImV4cCI6MjAxNTc3ODE4MX0.J0yA4SnftZnw7zHpKX8QCZIIMFin49R5fPjxJMAGiow')
 
 export default function Home() {
   const [authenticated, setAuthenticated] = useState(false);
--- src/components/Songs.tsx
+++ src/components/Songs.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { supabase } from '@/app/page'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://bpyktwagkbpnqtwlekfa.supabase.co'
+const supabase = createClient(supabaseUrl, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJweWt0d2Fna2JwbnF0d2xla2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMDIxODEsImV4cCI6MjAxNTc3ODE4MX0.J0yA4SnftZnw7zHpKX8QCZIIMFin49R5fPjxJMAGiow')
 
 const loadSongs = async() => {
   let { data: Songs, error } = await supabase
