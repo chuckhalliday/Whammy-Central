b89515cb519c59be54a61bab10872088fd947612
chuckhalliday <charleshallidayclarkii@gmail.com>
2023-11-20 03:05:15 -0800

    Add artist queries

--- src/components/Artists.tsx
+++ src/components/Artists.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://bpyktwagkbpnqtwlekfa.supabase.co'
+const supabase = createClient(supabaseUrl, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJweWt0d2Fna2JwbnF0d2xla2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMDIxODEsImV4cCI6MjAxNTc3ODE4MX0.J0yA4SnftZnw7zHpKX8QCZIIMFin49R5fPjxJMAGiow')
 
 const Artists = () => {
+
+  const [users, setUsers] = useState<any[]>([]);
+
+  const loadUsers = async () => {
+    let { data: users, error } = await supabase
+    .from('users')
+    .select()
+    if (users) {
+      setUsers(users)
+    }
+  }
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
   return (
-    <div>Artists</div>
+    <div>
+    <ul>
+      {users.map((user) => (
+        <li >{user.name ? user.name : user.email}</li>
+      ))}
+    </ul>
+  </div>
   )
 }
 
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://bpyktwagkbpnqtwlekfa.supabase.co'
+const supabase = createClient(supabaseUrl, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJweWt0d2Fna2JwbnF0d2xla2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMDIxODEsImV4cCI6MjAxNTc3ODE4MX0.J0yA4SnftZnw7zHpKX8QCZIIMFin49R5fPjxJMAGiow')
+
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const [artistName, setArtistName] = useState<string>('');
+  const [user, setUser] = useState<string>('')
 
-export default Profile
+  const getCurrentUser = async () => {
+    const { data: { user } } = await supabase.auth.getUser()
+    if (user){
+    setUser(user.id)
+    }
+  }
+
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setArtistName(event.target.value);
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await getCurrentUser()
+    const { data, error } = await supabase
+    .from('users')
+    .update({ name: artistName })
+    .eq('id', user)
+    .select()
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter your preferred name:
+          <input
+            className='text-box'
+            type="text"
+            value={artistName}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button className='button lightgreen' type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Profile;
